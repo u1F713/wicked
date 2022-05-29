e5c19431b6b3672324a841f9d9146d69944b2cac
Joseph Estevez <joseph.estevez@protonmail.com>
2022-05-29 17:06:19 -0400

    feat: support favicon

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import solidPlugin from 'vite-plugin-solid'
 export default defineConfig({
   plugins: [solidPlugin()],
   root: 'src',
+  publicDir: path.resolve(__dirname, 'public'),
   build: { target: 'esnext', outDir: path.resolve(__dirname, 'dist') },
   server: {
     port: 4444

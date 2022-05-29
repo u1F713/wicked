11779d968b3db09999c28c5ac5c04c6675e8d680
Joseph Estevez <joseph.estevez@protonmail.com>
2022-05-28 20:17:22 -0400

    feat build: setup out-dir
    move publish directory to <root>/dist.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solidPlugin()],
   root: 'src',
-  build: { target: 'esnext' },
+  build: { target: 'esnext', outDir: path.resolve(__dirname, 'dist') },
   server: {
     port: 4444
   }

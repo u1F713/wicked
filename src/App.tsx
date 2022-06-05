8e04ebe4a0b8898289b4790a5ff728d8ce6c336e
Joseph Estevez <joseph.estevez@protonmail.com>
2022-06-05 13:04:29 -0400

    feat: add variables sass

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Component, onCleanup, onMount } from 'solid-js'
 import Container from './components/Container'
 import Layout from './components/Layout'
 import { store, addPage, cleanStore } from './hooks/store'
-import './styles/global.sass'
+import './styles/main.sass'
 
 const App: Component = () => {
   onMount(() => {

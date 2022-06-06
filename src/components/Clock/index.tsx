94fa4162b2d328913541da5caccf0fa08223908e
Joseph Estevez <joseph.estevez@protonmail.com>
2022-06-06 07:30:09 -0400

    fix: components vite alias

--- src/components/Clock/index.tsx
+++ src/components/Clock/index.tsx
@@ -1,6 +1,6 @@
 import { Component } from 'solid-js'
-import useDateTime from '~/hooks/dateTime'
 import { ClockStyled } from './Clock.styled'
+import useDateTime from '~/hooks/dateTime'
 
 const Clock: Component = () => {
   const { getTime } = useDateTime()
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '~': resolve(__dirname, 'src'),
-      '@components': resolve(__dirname, 'src/view/components'),
+      '@components': resolve(__dirname, 'src/components'),
       '@styles': resolve(__dirname, 'src/views/styles')
     }
   },

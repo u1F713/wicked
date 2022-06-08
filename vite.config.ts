24a4b3f4f04320f709f19773528d55f0f9bfafa1
Joseph Estevez <joseph.estevez@protonmail.com>
2022-06-08 11:19:34 -0400

    feat: support vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), tsconfigPaths()],
   root: 'src',
   publicDir: resolve(__dirname, 'public'),
   build: { target: 'esnext', outDir: resolve(__dirname, 'dist') },

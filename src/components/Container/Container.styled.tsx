983baae00f1a053c683f18c95e44c366e1f54595
Joseph Estevez <joseph.estevez@protonmail.com>
2022-06-15 11:02:02 -0400

    feat: update layout

--- src/components/Container/Container.styled.tsx
+++ src/components/Container/Container.styled.tsx
@@ -2,9 +2,10 @@ import { styled } from 'solid-styled-components'
 
 export const Wrapper = styled('section')`
   display: flex;
+  place-items: start;
   flex-flow: column;
   gap: 1em;
   width: 100%;
-  max-width: 1100px;
-  margin: auto;
+  width: 700px;
+  max-width: 100%;
 `
--- src/views/home/components/Layout/Layout.styled.tsx
+++ src/views/home/components/Layout/Layout.styled.tsx
@@ -2,13 +2,18 @@ import { styled } from 'solid-styled-components'
 
 export const LayoutGrid = styled('div')`
   display: grid;
+  grid-template-columns: 1fr;
   min-height: 100vh;
   place-content: center;
   place-items: center;
-  gap: 2em;
+  gap: 3em;
 `
 
 export const FontDisplay = styled('h3')`
   font-size: 2.1em;
   font-weight: bold;
+`
+
+export const Section = styled('section')`
+  min-width: 80%;
 `
